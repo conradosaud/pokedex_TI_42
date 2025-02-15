c7d151419d62e7a860709cb22595e48ea8f0276b
Conrado Saud <50385963+conradosaud@users.noreply.github.com>
2025-02-14 22:12:39 -0300

    useEffect e lista de pokemons

--- app/page.js
+++ app/page.js
@@ -1,18 +1,40 @@
 'use client'
 
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
 
     const [ pokemons, alteraPokemons ] = useState([]);
     const [ pesquisa, alteraPesquisa ] = useState("");
 
+    const [ erroPesquisa, alteraErroPesquisa ] = useState(false);
+
     async function busca(){
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/pikachu")
-        const data = response.data;
-        console.log(data);
+        
+        try{
+            const response = await axios.get("https://pokeapi.co/api/v2/pokemon/"+pesquisa)
+    
+            const data = response.data;
+            alteraPokemons([data])
+            alteraErroPesquisa(false)
+        }catch(e){
+            alteraErroPesquisa(true);
+        }
+               
     }   
+
+    async function buscaTodos(){
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+        const data = response.data.results;
+        alteraPokemons(data)
+        console.log(data)
+    }
+
+
+    useEffect( ()=> {
+        buscaTodos();
+    }, [] )
 
     return (
         <div>
@@ -20,15 +42,26 @@ export default function Home() {
             <h1>Pokedex</h1>
 
             <p>Digite o nome de um Pokémon:</p>
-            <input/>
+            <input onChange={ (e)=> alteraPesquisa(e.target.value) } />
             <br/>
             <button onClick={ ()=> busca() } >Pesquisar</button>
 
+            { erroPesquisa == true && <p className="text-red-500" >Erro ao pesquisar</p> }
+
             <hr/>
 
-            <h2>Nome</h2>
-            <p> <strong>ID: </strong> ?? </p>
-            <img src=""/>
+            {
+                pokemons == 0 ?
+                    <p>Carregando...</p>
+                :
+                    pokemons.map( (i, index) => 
+                        <div>
+                            <h2> {i.name} </h2>
+                            <p> <strong>ID: </strong> { i.id ? i.id : index+1 } </p>
+                            <img src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/"+(i.id ? i.id : index+1)+".gif"} />
+                        </div>
+                    )
+            }
 
         </div>
     );
